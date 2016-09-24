a0aa418429b7300d149440cdccfb9e60e63eda36
dmlb2000 <dmlb2000@gmail.com>
2016-09-24 10:41:45 -0700

    add some checking bits

--- test-everything.sh
+++ test-everything.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -xe
 
-env
+env | sort
+which git
+echo "Did you add references to subprojects into this one?"
+if git diff $TRAVIS_COMMIT_RANGE | grep -iq myemsl ; then
+  echo "yes"
+else
+  echo "no"
+fi
